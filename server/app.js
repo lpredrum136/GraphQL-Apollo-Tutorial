68efe261528591870976679e7edc081012d9b0c7
Henry Nguyen <nguyenhai1591@gmail.com>
2021-02-14 10:05:09 +1000

    prepare deploy

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
 const mongoose = require('mongoose')
-<<<<<<< HEAD
-const path = require('path')
-=======
->>>>>>> 4d00b9622ffc911dc8083efcd20563b19f57c2e5
 
 // Load schema & resolvers
 const typeDefs = require('./schema/schema')
@@ -16,15 +12,12 @@ const mongoDataMethods = require('./data/db')
 // Connect to MongoDB
 const connectDB = async () => {
 	try {
-		await mongoose.connect(
-			process.env.MONGODB_URL,
-			{
-				useCreateIndex: true,
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-				useFindAndModify: false
-			}
-		)
+		await mongoose.connect(process.env.MONGODB_URL, {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false
+		})
 
 		console.log('MongoDB connected')
 	} catch (error) {
@@ -44,21 +37,6 @@ const server = new ApolloServer({
 const app = express()
 server.applyMiddleware({ app })
 
-<<<<<<< HEAD
-// Serve static assets if in production
-if (process.env.NODE_ENV == 'production') {
-	// Set static folder
-	app.use(express.static(path.join(__dirname, '../client/build')))
-
-	// Any request that is not /api/items should load index.html
-	app.get('*', (req, res) =>
-		res.sendFile(path.join(__dirname, '../client/build/index.html'))
-	)
-}
-
 app.listen({ port: process.env.PORT || 4000 }, () =>
-=======
-app.listen({ port: 4000 }, () =>
->>>>>>> 4d00b9622ffc911dc8083efcd20563b19f57c2e5
 	console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
 )
